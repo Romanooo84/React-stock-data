e52ec903dd4612b12351a8baa81b4e233cf94e4c
Pisarski Roman <pisarski.roman@gmail.com>
2024-08-21 14:09:54 +0200

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,20 +14,29 @@ export const Header=()=>{
     const [selectedTicker,setSelectedTicker]=useState('')
 
     
-    const onChange = (selectedOption) => {
-        setSelectedTicker(selectedOption.value)
-        console.log('Selected option:', selectedOption.value);
+    const onChange = (selectedOption, index) => {
+        const newTicker = selectedOption.value
+        const previousTicker = index.name
+        const tickerIndex = tickerList.indexOf(previousTicker);
+        const newTickerList=tickerList.map((ticker, index)=>{
+            if(tickerIndex===index){
+                return newTicker
+            }else{
+                return ticker
+            }
+        })
+        setTickerList(newTickerList)
     }
     
     const onInputChange = (event) => {
         setSearch(event)
     }
 
-    const handleClick = (index) => {
-        console.log('Clicked div with name:', index);
+   /* const handleClick = (index) => {
+        
         console.log(tickerList[index]);
         console.log(selectedTicker)
-    };
+    };*/
 
     useEffect(() => {
         multiplyData(tickerList)
@@ -59,8 +68,8 @@ export const Header=()=>{
      useEffect(() => {
         if (multiplyList.length > 0) {
             const markup = multiplyList.map((ticker, index) => (
-                <div key={index} name={index} onClick={() => handleClick(index)}>
-                    <Select placeholder="write min 3 letters..." options={options} onChange={onChange} onInputChange={onInputChange}/>
+                <div key={index} name={index}>
+                    <Select name={ticker.code} placeholder="write min 3 letters..." options={options} onChange={onChange} onInputChange={onInputChange}/>
                     <div>{ticker.code}</div>
                     <div>{ticker.change_p}%</div>
                 </div>
