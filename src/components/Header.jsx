ac37d162cca79c473eb5876f867e886a4af84472
Pisarski Roman <pisarski.roman@gmail.com>
2024-08-21 12:14:02 +0200

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react"
 import Select from 'react-select';
 import tickers from '../data/ticers'
+import { multiplyData } from '../hooks/downloadData';
 
-export const Header=({multiplyList})=>{
+export const Header=()=>{
 
     const [list, setList] = useState()
+    const [tickerList, setTickerList] = useState(['AAPL.US', 'EUR.FOREX'])
     const [search, setSearch] = useState()
     const [searchTerm, setSearchTerm]=useState()
     const [options, setOptions] = useState([{ label: '3', value: 'initial' }]);
-
+    const [multiplyList, setMultiplyList] = useState([])
 
     useEffect(() => {
         const results = tickers.filter(item => item.Name.includes(searchTerm));
@@ -29,17 +31,18 @@ export const Header=({multiplyList})=>{
         setSearch(event)
     }
 
+    const handleClick = (index) => {
+        console.log('Clicked div with name:', index);
+    };
+
     useEffect(() => {
-        if (multiplyList.length > 0) {
-            const markup = multiplyList.map((ticker, index) => (
-                <div key={index}>
-                    <div>{ticker.code}</div>
-                    <div>{ticker.change_p}%</div>
-                </div>
-            ));
-            setList(markup);
-        }
-    }, [multiplyList]);
+        multiplyData(tickerList)
+          .then(downloadedData => {
+            if (downloadedData) {
+              setMultiplyList(downloadedData);
+            }
+          });
+      }, []);
 
     useEffect(() => {
         if (search && search.length > 2) {
@@ -47,13 +50,25 @@ export const Header=({multiplyList})=>{
         }
     })
 
+     useEffect(() => {
+        if (multiplyList.length > 0) {
+            const markup = multiplyList.map((ticker, index) => (
+                <div key={index} name={index} onClick={() => handleClick(index)}>
+                    <Select placeholder="write min 3 letters..." options={options} onChange={onChange} onInputChange={onInputChange}/>
+                    <div>{ticker.code}</div>
+                    <div>{ticker.change_p}%</div>
+                </div>
+            ));
+            setList(markup);
+        }
+    }, [multiplyList, options]);
+
     useEffect(() => {
         setOptions([{ label: 'write 3 letters', value: 'initial' }]);
      },[]);
 
     return(
         <header>
-            <Select placeholder="write min 3 letters..." options={options} onChange={onChange} onInputChange={onInputChange}/>
             <div>{list}</div>
         </header>
     )
