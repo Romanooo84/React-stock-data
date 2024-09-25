5749939ff3876fba06b1003c385e7a8ebd462801
Pisarski Roman <pisarski.roman@gmail.com>
2024-09-25 09:21:15 +0200

    commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,8 @@
+import css from '../styles/Contact.module.css'
+
 export const Contact = () => {
     return (
-        <div>
+        <div className={css.mainDiv}>
             <p>Name: Roman Pisarski</p>
             <p>Phone: <a href="tel:+48504707289">504-707-289</a></p>
             <p>Email: <a href="mailto:pisarski.roman@gmail.com">pisarski.roman@gmail.com</a></p>
